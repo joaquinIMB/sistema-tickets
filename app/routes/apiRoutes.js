a612b20587d5ba52ca0e089ae9e51c21a71dd688
unknown <joaquinimbriago@gmail.com>
2024-06-24 17:22:10 -0300

    Correccion de errores en produccion

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,4 +1,4 @@
 export const API_URL =
 process.env.NODE_ENV === "production"
   ? "https://sistema-tickets-pink.vercel.app/api/ticket"
-  : "http://127.0.0.1:3000/api/ticket";
+  : "http://localhost:3000/api/ticket";
