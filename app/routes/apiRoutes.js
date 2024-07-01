d015e9a31761b7e7c67fddc815366b6ca3f18f9a
unknown <joaquinimbriago@gmail.com>
2024-07-01 16:51:58 -0300

    Cambio en end point de produccion

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,9 +1,9 @@
 export const API_URL =
 process.env.NODE_ENV === "production"
-  ? "http://localhost:3000/api/ticket"
+  ? "https://sistema-tickets-pink.vercel.app/api/ticket"
   : "http://localhost:3000/api/ticket";
 
 export const API_URL_SERVICES = 
 process.env.NODE_ENV === "production" 
-? "http://localhost:3000/api/" 
+? "https://sistema-tickets-pink.vercel.app/api/" 
 : "http://localhost:3000/api/"
